967121f554f37041f0a5befbf56ee41e0957558f
praveen <praveen.chundi13@gmail.com>
2017-04-27 00:50:47 -0500

    user sign up module

--- app/flask/sbw-services.py
+++ app/flask/sbw-services.py
@@ -18,52 +18,58 @@ CORS(app)
 #     print row[1], "-->", row[2]
 
 
-users = [
-            {
-                "userId": "RahulDev",
-                "passCode": "123456789"
-            },
-            {
-                "userId": "praveenDev",
-                "passCode": "987654321"
-            },
-            {
-                "userId": "praveenDev",
-                "passCode": "987654321"
-            }
-        ]
+# users = [
+#             {
+#                 "userId": "RahulDev",
+#                 "passCode": "123456789"
+#             },
+#             {
+#                 "userId": "praveenDev",
+#                 "passCode": "987654321"
+#             },
+#             {
+#                 "userId": "praveenDev",
+#                 "passCode": "987654321"
+#             }
+#         ]
 success =   {
                 'status': "Success",
-                'errorDesc': "User verified",
+                'errorDesc': "User details has been succesfully inserted",
                 'statusCode': 200
             }
 
 error = {
-            'status': "errored",
-            'errorDesc': "User not verified",
+            'status': "Failure",
+            'errorDesc': "User already have an account",
             'statusCode': 200
         }
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    return jsonify({'users': users})
+# @app.route('/users', methods=['GET'])
+# def get_users():
+#     return jsonify({'users': users})
 
 @app.route('/setuser', methods=['POST'])
 def set_user():
-    db = MySQLdb.connect(host="localhost", user="root", passwd="Baba321gow.",db="sbw_db")
-    cursor = db.cursor()
-    cursor.execute("SELECT passCode, userId FROM sbw_db.sbw_users")
-    db.commit()
-    result_set = cursor.fetchall()
     reqJson = request.get_json(force=True)
     print 'data from client:', reqJson
+    db = MySQLdb.connect(host="localhost", user="root", passwd="Baba321gow.",db="sbw_db")
+    cursor = db.cursor()
+    cursor.execute("SELECT email FROM sbw_db.sbw_users")
+    result_set = cursor.fetchall()
+    print '====>', result_set
+    user_exist = False
     for row in result_set:
-        print row[0] ,'---->', row[1]
-        if reqJson['userId'] == row[1] and reqJson['passCode'] == row[0]:
-            dictToReturn = success
+        if reqJson['email'] == row[0]:
+            dictToReturn = error
+            user_exist = True
             break
         else:
-            dictToReturn = error
+            user_exist = False
+    if user_exist == False:
+        cursor.execute("""INSERT INTO sbw_db.sbw_users (email, firstname, lastname, passcode)VALUES (%s, %s, %s, %s)""", (reqJson['email'], reqJson['firstName'], reqJson['lastName'], reqJson['passcode']))
+        dictToReturn = success
+
+    db.commit()
     return jsonify(dictToReturn)
 
 if __name__ == '__main__':
