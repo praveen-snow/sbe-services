b568a61e4a2f678306e1f7111c09c850d63151e3
praveen <praveen.chundi13@gmail.com>
2017-04-28 02:07:04 -0500

    common db connection

--- app/flask/sbw-services.py
+++ app/flask/sbw-services.py
@@ -6,26 +6,23 @@ app = Flask(__name__)
 CORS(app)
 success =   {
                 'status': "Success",
-                'errorDesc': "User details has been succesfully inserted",
-                'statusCode': 200
+                'errorDesc': "User details has been succesfully inserted"
             }
 
 error = {
             'status': "Failure",
-            'errorDesc': "User already have an account",
-            'statusCode': 200
+            'errorDesc': "User already have an account"
         }
 
-
+db = MySQLdb.connect(host="localhost", user="root", passwd="Baba321gow.",db="sbw_db")
+cursor = db.cursor()
 @app.route('/user/signin', methods=['POST'])
 def user_sigin():
-    db = MySQLdb.connect(host="localhost", user="root", passwd="Baba321gow.",db="sbw_db")
-    cursor = db.cursor()
     reqJson = request.get_json(force=True)
-    print 'data from client:', reqJson
+    #print 'data from client:', reqJson
     cursor.execute("SELECT email, passcode FROM sbw_db.sbw_users")
     result_set = cursor.fetchall()
-    print '====>', result_set
+    #print '====>', result_set
     user_exist = False
     for row in result_set:
         if reqJson['email'] == row[0] and reqJson['passcode'] == row[1]:
@@ -34,17 +31,14 @@ def user_sigin():
             break
         else:
             dictToReturn = error
-            dictToReturn['errorDesc'] = "User details not found"
-            
+            dictToReturn['errorDesc'] = "User details not found" 
     db.commit()
     return jsonify(dictToReturn)
 
 @app.route('/user/signup', methods=['POST'])
 def user_signup():
-    db = MySQLdb.connect(host="localhost", user="root", passwd="Baba321gow.",db="sbw_db")
-    cursor = db.cursor()
     reqJson = request.get_json(force=True)
-    print 'data from client:', reqJson
+    #print 'data from client:', reqJson
     cursor.execute("SELECT email FROM sbw_db.sbw_users")
     result_set = cursor.fetchall()
     # print '====>', result_set
